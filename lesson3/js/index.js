1aeabe6f0c4127e01f4ed47b4d30b0628c5676a7
Patten Whiting <whi20045@byui.edu>
2021-01-25 22:13:05 -0700

    changes everything to camelcase, updated titles and descriptions

--- lesson3/js/index.js
+++ lesson3/js/index.js
@@ -1,4 +1,4 @@
 var x = document.lastModified;
 var year = new Date().getFullYear();
-document.getElementById('LastModified').innerHTML = x;
+document.getElementById('lastModified').innerHTML = x;
 document.getElementById('copyright').innerHTML = year;
