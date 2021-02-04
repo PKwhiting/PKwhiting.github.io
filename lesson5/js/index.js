c2fe3fe902f0c05bd77372a9b2ec8da739b5948c
Patten Whiting <whi20045@byui.edu>
2021-02-04 14:22:47 -0700

    added js for announcement

--- lesson5/js/index.js
+++ lesson5/js/index.js
@@ -6,3 +6,15 @@ document.getElementById('copyright').innerHTML = year;
 const hambutton = document.querySelector('.hamburger');
 const mainnav = document.querySelector('.navigation');
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+var y = document.getElementsByClassName('announcement');
+var day = new Date();
+var daynum = day.getDay();
+console.log(daynum);
+if (daynum == 5 ){
+    document.getElementById('announcement').style.display = 'block';
+}
+else {
+    document.getElementById('announcement').style.display = 'none';
+}
+
