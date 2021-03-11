846f44be36318a040bf83e9f6755845dedf8b98c
Patten Whiting <whi20045@byui.edu>
2021-03-10 18:08:44 -0700

    finsihed JSON data for info card

---
+++ Lesson10/js/prophets.js
@@ -0,0 +1,30 @@
+const requestURL = "https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json";
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let span1 = document.createElement('p')
+        let span2 = document.createElement('p')
+        let img = document.createElement('img');
+
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        span1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+        span2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        img.src = prophets[i].imageurl;
+        card.appendChild(h2);
+        card.appendChild(span1);
+        card.appendChild(span2);
+        card.appendChild(img);
+
+        document.querySelector('div.cards').appendChild(card);
+
+    }
+  });
+
+  
