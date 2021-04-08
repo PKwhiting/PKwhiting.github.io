16012fde626b26b6ff728436eb31abc2f51dc766
Patten Whiting <whi20045@byui.edu>
2021-04-07 23:50:53 -0600

    validation errors

--- final-project/js/directory.js
+++ final-project/js/directory.js
@@ -8,13 +8,24 @@ fetch(apiURL)
     for (i = 0; i < 8; i++) {
         let card = document.getElementById("cards");
         let obj = card.children[i];
-        obj.children[0].src = jsObject.businesses[i].logo;
-        obj.children[1].innerHTML= jsObject.businesses[i].businessName;
+        obj.children[1].src = jsObject.businesses[i].logo;
+        obj.children[1].alt = jsObject.businesses[i].businessName + " logo";
+        obj.children[0].innerHTML= jsObject.businesses[i].businessName;
         obj.children[2].innerHTML= jsObject.businesses[i].businessAddress;
         obj.children[3].innerHTML= jsObject.businesses[i].phoneNumber;
         obj.children[4].innerHTML= jsObject.businesses[i].businessWebsite;
+        obj.children[4].href= jsObject.businesses[i].businessWebsite;
         obj.children[5].innerHTML= jsObject.businesses[i].businessDesription;
 
     }
 
   });
+
+let grid = document.getElementById("grid");
+let list = document.getElementById('list');
+grid.onclick = function changeContent(){
+  document.getElementById("cards").style.gridTemplateColumns = "auto auto";
+}
+list.onclick = function changeContent(){
+  document.getElementById("cards").style.gridTemplateColumns = "auto";
+}
